f2ffc39987b6d29f4d625047098a668a033057f4
siddu <annamsiddeshwereddy2006@gmail.com>
2026-03-06 17:16:27 +0530

    small fix

--- frontend/components/PricingModal.jsx
+++ frontend/components/PricingModal.jsx
@@ -4,19 +4,19 @@ import React,{useState} from 'react'
 import {Dialog, DialogContent, DialogTrigger, DialogTitle } from './ui/dialog'
 import PricingSection from './PricingSection'
 
-const PricingModal = ({subscriptionTier="free",children}) => {
-    const [isOpen, setIsOpen]=useState(false);
+const PricingModal = ({ subscriptionTier = "free", children }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const canOpen=subscriptionTier==="free";
-    return (
-        <Dialog open={isOpen} onOpenChange={canOpen?setIsOpen:undefined}>
-            <DialogTrigger>{children}</DialogTrigger>
-            <DialogContent className="p-8 pt-4 sm:max-w-4xl">
-                <DialogTitle />
-                <PricingSection />
-            </DialogContent>
-        </Dialog>
-    )
-}
+  const canOpen = subscriptionTier === "free";
+  return (
+    <Dialog open={isOpen} onOpenChange={canOpen ? setIsOpen : undefined}>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="p-8 pt-4 sm:max-w-4xl">
+        <DialogTitle />
+        <PricingSection />
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export default PricingModal
